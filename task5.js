5d62c351b19ae7285c9f204edbc2682f3fdc8576
David Matiashvili <70051407+dav26072000@users.noreply.github.com>
2021-04-28 18:03:00 +0400

    solve number submit task

--- task5.js
+++ task5.js
@@ -3,6 +3,19 @@ function twoNumbers(num1, num2) {
   return (result = num1 + num2);
 }
 console.log(twoNumbers(1, 2));
+
 // 6.35
 alert(Math.round(6.35 * 10) / 10); // 6.4
-//
+
+//number submit
+function numSubmit(num) {
+  if (typeof num === "number") {
+    return num;
+  } else if (num === "cancel") {
+    return "canceled";
+  } else {
+    return false;
+  }
+}
+
+console.log(numSubmit("d"));
