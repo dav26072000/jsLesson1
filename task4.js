be1b141d0b3aba74deb870ad8c06749f79fc5c9a
David Matiashvili <70051407+dav26072000@users.noreply.github.com>
2021-04-28 17:12:28 +0400

    solve age task 2 and if task

--- task4.js
+++ task4.js
@@ -4,6 +4,7 @@ alert(1 && null && 2); // null
 alert(alert(1) && alert(2)); // 1 then undefined
 alert(null || (2 && 3) || 4); // 2 && 3 has preority then null || 3 || 4 = 3
 
+// age task 1
 function ageFunction(age) {
   if (age >= 14 && age <= 90) {
     console.log(true);
@@ -11,5 +12,19 @@ function ageFunction(age) {
     console.log(false);
   }
 }
-
 ageFunction();
+// age task 2
+function ageFunction2(age) {
+  if (!(age >= 14 && age <= 90)) {
+    console.log(true);
+  } else {
+    console.log(false);
+  }
+}
+ageFunction2();
+
+// if task
+
+if (-1 || 0) alert("first"); // -1 = true => true
+if (-1 && 0) alert("second"); // 0 = false => false
+if (null || (-1 && 1)) alert("third"); // -1 = true 1 = true => true
