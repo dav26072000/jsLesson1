b443d4ba0fa6acd0a6fc793a678253c9e5904322
David Matiashvili <70051407+dav26072000@users.noreply.github.com>
2021-04-28 16:04:55 +0400

    answers is in comment

---
+++ task3.js
@@ -0,0 +1,7 @@
+5 > 4; // true
+"apple" > "pineapple"; // false
+"2" > "12"; // false
+undefined == null; // false
+undefined === null; // false
+null == "\n0\n"; // false
+null === +"\n0\n"; // false
